edb219c74959689a68ae8a27617cb843444f5057
14H <doduchieu.kstn@gmail.com>
2022-08-09 15:18:43 +0700

    Update sleep when craw shopeee err

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ import { crawTiki } from "./tiki/index.js";
 import { crawShopee } from "./shopee/index.js";
 import { crawLazada } from "./lazada/index.js";
 import { prepareDb } from "./db.js";
-const platform = process.env.POSTGRES_PLATFORM ?? 'all'
+const platform = process.env.PLATFORM ?? 'all'
 async function main() {
     await prepareDb()
     switch (platform) {
